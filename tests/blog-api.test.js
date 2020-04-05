d67bd750582126c5c01e0e48b6b6267fd15f514d
Dastan <dastanbeksamatov@gmail.com>
2020-04-05 22:11:50 +0300

    Refactored the code for testing

--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -13,75 +13,79 @@ beforeEach(async () => {
     await blogObj.save()
   }
 })
+describe('When there is initially saved blogs ', () => {
+  describe('blogs can be viewed', () => {
+    test('It returns blogs in JSON', async () => {
+      await api
+        .get('/api/blogs')
+        .expect('Content-Type', /application\/json/)
+    })
+    test('The amount of blogs in the db ', async () => {
+      const response = await api.get('/api/blogs')
+      expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+  })
+  describe('checks if the id property is defined', () => {
+    test('Unique identifier is defined ', async () => {
+      const response = await api.get('/api/blogs')
+      expect(response.body[1].id).toBeDefined()
+    })
+  })
+  describe('adding a blog in the db', () => {
+    test('New blog can be added ', async () => {
+      const newBlog = {
+        title: 'Why should we do testing',
+        author: 'Dastan Samatov',
+        url: 'https://dastan.sh/blog/why_testing',
+        likes: 10
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+      const titles = blogsAtEnd.map(blog => blog.title)
+      expect(titles).toContain('Why should we do testing')
+    })
 
-test('It returns blogs in JSON', async () => {
-  await api
-    .get('/api/blogs')
-    .expect('Content-Type', /application\/json/)
-})
-test('The amount of blogs in the db ', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+    test('If object has no property [likes], init and default it to 0 ', async () => {
+      const newBlog = {
+        title: 'Why should we learn React',
+        author: 'Dastan Samatov',
+        url: 'https://dastan.sh/blog/why_React',
+      }
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+      const blogsAtEnd = await helper.blogsInDb()
+      const savedBlog = blogsAtEnd[helper.initialBlogs.length]
+      expect(savedBlog.likes).toBeDefined()
+      expect(savedBlog.likes).toBe(0)
+    })
 
-test('Unique identifier is defined ', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body[1].id).toBeDefined()
+    test('if url and title props are missing, expect bad request', async () => {
+      const testBlog1 = {
+        author: 'Dastan Samatov',
+        likes: 12
+      }
+      const testBlog2 = {
+        title: 'How Jest works',
+        author: 'John Smith',
+        likes:11
+      }
+      await api
+        .post('/api/blogs')
+        .send(testBlog1)
+        .expect(400)
+      await api
+        .post('/api/blogs')
+        .send(testBlog2)
+        .expect(201)
+    })
+  })
 })
-
-test('New blog can be added ', async () => {
-  const newBlog = {
-    title: 'Why should we do testing',
-    author: 'Dastan Samatov',
-    url: 'https://dastan.sh/blog/why_testing',
-    likes: 10
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
-  const titles = blogsAtEnd.map(blog => blog.title)
-  expect(titles).toContain('Why should we do testing')
-})
-
-test('If object has no property [likes], init and default it to 0 ', async () => {
-  const newBlog = {
-    title: 'Why should we learn React',
-    author: 'Dastan Samatov',
-    url: 'https://dastan.sh/blog/why_React',
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-  const blogsAtEnd = await helper.blogsInDb()
-  const savedBlog = blogsAtEnd[helper.initialBlogs.length]
-  expect(savedBlog.likes).toBeDefined()
-  expect(savedBlog.likes).toBe(0)
-})
-
-test('if url and title props are missing, expect bad request', async () => {
-  const testBlog1 = {
-    author: 'Dastan Samatov',
-    likes: 12
-  }
-  const testBlog2 = {
-    title: 'How Jest works',
-    author: 'John Smith',
-    likes:11
-  }
-  await api
-    .post('/api/blogs')
-    .send(testBlog1)
-    .expect(400)
-  await api
-    .post('/api/blogs')
-    .send(testBlog2)
-    .expect(201)
-})
-
 afterAll(() => {
   mongoose.connection.close()
 })
